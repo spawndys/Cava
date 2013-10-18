7c85899e11d3a2ace9e4678d1888f559e74de6d2
daniel brandes <bradan1@umbc.edu>
2013-10-18 15:43:24 -0400

    Added Java Code

---
+++ Java/Coolgle/src/coolgle/FileManager.java
@@ -0,0 +1,17 @@
+package coolgle;
+/*****************************************
+ ** File: KMlCreator
+ ** Team Name: Cava++
+ *Date: 10/18/13
+ ** E-mail: Daniel Brandes bradan1@umbc.edu, 
+ ** Lizset Chavez <lizset1@umbc.edu>
+ ** Patrick Ritchie <ritc1@umbc.edu>,
+ ** Xiaofei He <xiaofei2@umbc.edu>,
+ ** Yo-Han Kim <ykim18@umbc.edu>,
+ ** Jim Millican <jmill1@umbc.edu>
+ ** Decription- File that Parses Search into a txt file
+ ** and a txt File into search
+***********************************************/
+public class FileManager {
+
+}
---
+++ Java/Coolgle/src/coolgle/UserAuthentication.java
@@ -0,0 +1,17 @@
+package coolgle;
+/*****************************************
+ ** File: UserAuthentication
+ ** Team Name: Cava++
+ *Date: 10/18/13
+ ** E-mail: Daniel Brandes bradan1@umbc.edu, 
+ ** Lizset Chavez <lizset1@umbc.edu>
+ ** Patrick Ritchie <ritc1@umbc.edu>,
+ ** Xiaofei He <xiaofei2@umbc.edu>,
+ ** Yo-Han Kim <ykim18@umbc.edu>,
+ ** Jim Millican <jmill1@umbc.edu>
+ ** Decription- File that simulates an authenticator to application
+ ** for a user
+***********************************************/
+public class UserAuthentication {
+
+}
