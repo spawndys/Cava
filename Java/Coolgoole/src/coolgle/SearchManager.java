ecb15fbcc5ac5377490398f72cd02210f688c2cf
jimbodoken <jmill1@umbc.edu>
2013-11-05 22:26:51 -0500

    Changes to files to better match design document.

---
+++ Java/Coolgoole/src/coolgle/SearchManager.java
@@ -0,0 +1,18 @@
+package coolgle;
+
+public class SearchManager {
+	
+	/**
+	 * sortShortestDistance
+	 * Description- 
+	 * Pre- 
+	 * Post -
+	 */
+	
+	public static void sortShortestDistance(Search search){
+		//get start and end locations from search
+		//sort locations start-end with shortest distance 
+		
+		
+	}
+}
---
+++ Java/Coolgoole/src/coolgle/UIManager.java
@@ -0,0 +1,49 @@
+package coolgle;
+/*****************************************
+ ** File: Gui
+ ** Team Name: Cava++
+ *Date: 10/18/13
+ ** E-mail: Daniel Brandes bradan1@umbc.edu, 
+ ** Lizset Chavez <lizset1@umbc.edu>
+ ** Patrick Ritchie <ritc1@umbc.edu>,
+ ** Xiaofei He <xiaofei2@umbc.edu>,
+ ** Yo-Han Kim <ykim18@umbc.edu>,
+ ** Jim Millican <jmill1@umbc.edu>
+ ** Decription- Interface for user to use to interact with
+***********************************************/
+public class UIManager {
+	/**
+	 * displayLogInGui()
+	 * Description- This will be a function to load login screen
+	 * Pre-
+	 * Post-
+	 */
+	private void displayLogInGui(){
+		
+	}
+	/**displayUserGui()
+	 * Description- This will be a function to load an user screen
+	 * Pre-
+	 * Post-
+	 */
+	private void displayUserGui(){
+		
+	}
+	/**displayAdminGui()
+	 * Description- This will be a function to load an admin screen
+	 * Pre-
+	 * Post-
+	 */
+	private void displayAdminInGui(){
+	
+	}
+	/**
+	 * displaySearchGui()
+	 * Description- This will be a function to load a search screen
+	 * Pre-
+	 * Post-
+	 */
+	private void displaySearchInGui(){
+		
+	}
+}
